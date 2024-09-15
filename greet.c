1a176bb374555bf46931279928349227e3e27bec
NIKHIL8770 <nikhilprajapati870@gmail.com>
2024-09-15 18:19:16 +0530

    Update greet.c

--- greet.c
+++ greet.c
@@ -1,24 +1,24 @@
-// #include<stdio.h>
-// void nikhil(){//5
-//   printf("chotu\n");
-// }
+#include<stdio.h>
+void nikhil(){//5
+  printf("chotu\n");
+}
 
-// void greet(){ //3
-//   nikhil(); //4
-//   int a;
-//   a=4;
-//   printf("%d",a);
-// }
-// int main(){   //1
-//  printf("hello, how are you\n");
-//   greet(); //2
-// printf("\n");
-//   printf("\nHELLO\n");
-//    greet(); 
-//   greet(); 
-//   greet();
-//   greet();
-//   greet();
+void greet(){ //3
+  nikhil(); //4
+  int a;
+  a=4;
+  printf("%d",a);
+}
+int main(){   //1
+ printf("hello, how are you\n");
+  greet(); //2
+printf("\n");
+  printf("\nHELLO\n");
+   greet(); 
+  greet(); 
+  greet();
+  greet();
+  greet();
   
-// return 0;
-// }
+return 0;
+}
