dfe16da74e5d3097f77baf7d7fea8a73085578dc
NIKHIL8770 <nikhilprajapati870@gmail.com>
2024-09-15 15:42:21 +0530

    Add files via upload
    all are related to function

---
+++ parameters.c
@@ -0,0 +1,15 @@
+// #include<stdio.h>
+// void hello(int z){
+// if(z%2==0) printf("%deven number",z);
+// else printf("%d odd number",z);
+// }
+
+
+
+// int main(){
+//   int a; 
+//   scanf("%d",&a);
+//   hello(a);
+
+// return 0;
+// }
---
+++ retuentype.c
@@ -0,0 +1,16 @@
+// #include<stdio.h>
+// int min(int x , int y){
+//   if(x<y) return x;
+//   else return y;
+// }
+// int main(){
+//   int a;
+//   printf("enter a number a  :");
+//   scanf("%d",&a);
+//   int b;
+//   printf("enter a number b  :");
+//   scanf("%d",&b);
+// int m= min(a,b);
+//   printf("min of %d and %d is %d",a,b,m);
+// return 0;
+// }
